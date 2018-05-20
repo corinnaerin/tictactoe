6100122d80359488e5d58586b48baa384ea8e5b2
Corinna Kuhn <corinnaerin@gmail.com>
2018-05-20 12:48:18 -0700

    Add nyc/istanbul for code coverage

--- test/client/index.test.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable only-arrow-functions */
-import { expect } from 'chai';
-
-describe('Placeholder test', function() {
-  it('should pass', function() {
-    const a = true;
-    expect(a).to.be.true;
-  });
-});
